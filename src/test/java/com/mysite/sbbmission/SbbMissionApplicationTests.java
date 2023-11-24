d1441832af06d4f50d78404e4dfd7198bed341e2
silvercho <tjddms01212@gmail.com>
2023-11-24 16:58:22 +0900

    2-07

--- src/test/java/com/mysite/sbbmission/SbbMissionApplicationTests.java
+++ src/test/java/com/mysite/sbbmission/SbbMissionApplicationTests.java
@@ -1,26 +1,38 @@
 package com.mysite.sbbmission;
 
+import com.mysite.sbbmission.answer.Answer;
+import com.mysite.sbbmission.answer.AnswerRepository;
 import com.mysite.sbbmission.question.Question;
 import com.mysite.sbbmission.question.QuestionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbMissionApplicationTests {
     @Autowired
     private QuestionRepository questionRepository;
 
+    @Autowired
+    private AnswerRepository answerRepository;
     @Test
     void testJpa() {
-        List<Question> all = this.questionRepository.findAll();
-        assertEquals(2, all.size());
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
 
-        Question q = all.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
+        Answer a = new Answer();
+        a.setContent("네 자동으로 생성됩니다.");
+        a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+        a.setCreateDate(LocalDateTime.now());
+        this.answerRepository.save(a);
     }
 }
