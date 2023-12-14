e2624999e053f43fd1ff30146fe1ce4b2e4ae6a0
silvercho <tjddms01212@gmail.com>
2023-12-14 23:24:51 +0900

    3-10

--- src/main/java/com/mysite/sbbmission/question/QuestionService.java
+++ src/main/java/com/mysite/sbbmission/question/QuestionService.java
@@ -1,6 +1,7 @@
 package com.mysite.sbbmission.question;
 
 import com.mysite.sbbmission.DataNotFoundException;
+import com.mysite.sbbmission.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -33,12 +34,23 @@ public class QuestionService {
         }
     }
 
-    public void create(String subject, String content) {
+    public void create(String subject, String content , SiteUser user) {
         Question q = new Question();
         q.setSubject(subject);
         q.setContent(content);
         q.setCreateDate(LocalDateTime.now());
+        q.setAuthor(user);
         this.questionRepository.save(q);
     }
 
+    public void modify(Question question, String subject, String content) {
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setModifyDate(LocalDateTime.now());
+        this.questionRepository.save(question);
+    }
+
+    public void delete(Question question) {
+        this.questionRepository.delete(question);
+    }
 }
